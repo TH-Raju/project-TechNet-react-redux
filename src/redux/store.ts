9696df6bc3b247fb3ece041dfc80c1e151dfb9e6
TH-Raju <rajukhan894200@gmail.com>
2023-08-07 23:48:16 +0600

    [Done] project complete and practice going on :)

---
+++ src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
+import { api } from './api/apiSlice';
+
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+        product: productReducer,
+        [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
